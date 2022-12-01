da29b3e1f6cfdcfa4e4a36f00aef369ae52a23ed
alex <linxam@gmail.com>
2022-12-01 08:36:04 +0300

    Function get_currencies was added

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 import telebot
 import requests
+import json
+import os
+import datetime
 
-#TODO: получить данные о валютах
-URL = 'https://www.cbr-xml-daily.ru/daily_json.js'
-a = requests.get(URL)
-if a.status_code == 200:
-    b = open(a)
+def get_currencies():
+
+    currencies = []
+
+    URL = 'https://www.cbr-xml-daily.ru/daily_json.js'
+
+    now = datetime.datetime.now()
+    filename = f'{now.year}-{now.month:02d}-{now.day:02d}_currencies.json'
+
+    if not os.path.exists(filename):
+        a = requests.get(URL)
+
+        if a.status_code == 200:
+            currencies = json.loads(a.text)
+
+            with open(filename, 'w', encoding='utf-8') as f:
+                json.dump(currencies, f, ensure_ascii=False, indent=4)
+
+            files_to_remove = [file for file in os.listdir() if file.endswith('_currencies.json') and file != filename]
+            for i in files_to_remove:
+                os.remove(i)
+
+    else:
+        with open(filename, 'r', encoding='utf-8') as f:
+            currencies = json.load(f)
+
+    return currencies
+
+currencies = get_currencies()
+
