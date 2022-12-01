4f23700d9cb121261f3a03ffb0b3ac15e14aeaef
kloch <klochkov.fedos@gmail.com>
2022-11-30 20:48:24 -0800

    URl add

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import telebot
 import requests
 
 #TODO: получить данные о валютах
-URl = 'https://www.cbr-xml-daily.ru/daily_json.js'
+URL = 'https://www.cbr-xml-daily.ru/daily_json.js'
+a = requests.get(URL)
+if a.status_code == 200:
+    b = open(a)
