1406f1142f23fd8de9e5e151f4f27486c43180fe
kloch <klochkov.fedos@gmail.com>
2022-12-05 15:49:22 -0800

    URl add

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ import requests
 import json
 import os
 import datetime
+import emoji
+
+
+# token = '5361815767:AAGwQlqlJZAXqyJ6R8Igw1fXdaaZkVskOpw'
+#
+# bot = telebot.TeleBot(token)
+#
+# @bot.message_handler(commands=['start', 'help'])
+# def helpas(message):
+#     helps = '''
+#
+#     '''
+
+
+
 
 def get_currencies():
 
@@ -30,7 +45,56 @@ def get_currencies():
         with open(filename, 'r', encoding='utf-8') as f:
             currencies = json.load(f)
 
+    if currencies:
+        currencies = currencies["Valute"]
+
+
     return currencies
 
-currencies = get_currencies()
 
+def list_currencies(currencies):
+    flags = {
+        'AUD': ":Australia:",
+        'AZN': ":Azerbaijan:",
+        'GBP': ":United_Kingdom:",
+        "AMD": ':Armenia:',
+        "BYN": ':Belarus:',
+        "BGN": ':Bulgaria:',
+        "BRL": ':Brazil:',
+        "HUF": ':Hungary:',
+        "HKD": ':Hong_Kong_SAR_China:',
+        "DKK": ':Denmark:',
+        "USD": '',
+        "EUR": ':Germany:',
+        "INR": ':India:',
+        "KZT": ':Kazakhstan:',
+        "CAD": ':Canada:',
+        "KGS": ':Kyrgyzstan:',
+        "CNY": ':China:',
+        "MDL": ':Moldova:',
+        "NOK": '',
+        "PLN": '',
+        "RON": ':Romania:',
+        "XDR": ':airplane_departure:',
+        "SGD": ':Singapore:',
+        "TJS": ':Tajikistan:',
+        "TRY": ':Turkey:',
+        "TMT": ':Turkmenistan:',
+        "UZS": ':Uzbekistan:',
+        "UAH": ':Ukraine:',
+        "CZK": ':Czechia:',
+        "SEK": ':Sweden:',
+        "CHF": ':Switzerland:',
+        "ZAR": ':South_Africa:',
+        "KRW": '',
+        "JPY": ':Japan:'
+    }
+    a = ''
+    for i in currencies:
+        a += (f'{i} {currencies[i]["Name"]} {currencies[i]["Nominal"]} {currencies[i]["Value"]} \n')
+    return a
+
+
+currencies = get_currencies()
+print(list_currencies(currencies))
+# print(emoji.emojize('	:Russia:'))
